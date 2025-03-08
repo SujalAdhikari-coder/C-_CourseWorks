7fa5a8ad9ed631356671ef23d0d8d61ab6cdf08c
sujal adhikari <sujaladhikari149@gmail.com>
2025-03-08 08:24:21 +0545

    ADDED COMMENTS IN TASK1.2.cpp

--- Worksheet 1/TASK1.2.cpp
+++ Worksheet 1/TASK1.2.cpp
@@ -1,19 +1,18 @@
-//1.2-Write a C++ program to implement a number guessing game with different difficulty levels.
 #include <iostream>
-#include <ctime>
+#include <ctime>  // For generating random numbers
 using namespace std;
 int main() {
-    srand(time(0));
-    int easy = rand() % 8 + 1;
-    int medium = rand() % 30 + 1;
-    int hard = rand() % 50 + 1;
+    srand(time(0));  // Seed the random number generator
+    int easy = rand() % 8 + 1;    // Random number between 1 and 8
+    int medium = rand() % 30 + 1; // Random number between 1 and 30
+    int hard = rand() % 50 + 1;   // Random number between 1 and 50
     int guess;
     char difficulty;
     cout << "Welcome to the Number Guessing Game!" << endl;
     cout << "Choose your difficulty: Easy (e), Medium (m), Hard (h): ";
     cin >> difficulty;
     switch (difficulty) {
-        case 'e': case 'E':  // Corrected syntax
+        case 'e': case 'E':
             cout << "Guess a number between 1 and 8: ";
             cin >> guess;
             if (guess == easy) {
