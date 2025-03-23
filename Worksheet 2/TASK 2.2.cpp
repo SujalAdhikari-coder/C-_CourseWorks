ecc7948e9c52fa374ccd42947846e4f0d14e298c
sujal adhikari <sujaladhikari149@gmail.com>
2025-03-23 23:59:25 +0545

    Final Update In TASK 2.2.cpp

--- Worksheet 2/TASK 2.2.cpp
+++ Worksheet 2/TASK 2.2.cpp
@@ -1,16 +1,16 @@
 //Create a program with these overloaded functions named findMax
 #include <iostream>
 using namespace std;
-class MaxFinder { // Class to handle the findMax function overloading
+class MaxFinder {
 public:
-    int findMax(int a, int b) { // Function to find maximum between two integers
-        return (a > b) ? a : b; // Return the greater of the two integers
+    int findMax(int a, int b) { //Function to find maximum between two integers
+        return (a > b) ? a : b; //Return the greater of the two integers
     }
-    float findMax(float a, float b) { // Function to find maximum between two floating-point numbers
-        return (a > b) ? a : b; // Return the greater of the two floats
-    }    
-        int findMax(int a, int b, int c) { // Function to find maximum among three integers
-        if (a > b && a > c) { // Compare all three integers and return the maximum
+    float findMax(float a, float b) { //Function to find maximum between two floating-point numbers
+        return (a > b) ? a : b; //Return the greater of the two floats
+    }
+        int findMax(int a, int b, int c) { //Function to find maximum among three integers
+        if (a > b && a > c) { //Compare all three integers and return the maximum
             return a;
         } else if (b > a && b > c) {
             return b;
@@ -18,13 +18,13 @@ public:
             return c;
         }
     }
-    float findMax(int a, float b) { // Function to find maximum between an integer and a floating-point number
-        return (a > b) ? a : b; // Return the greater of the integer and float
+    float findMax(int a, float b) { //Function to find maximum between an integer and a floating-point number
+        return (a > b) ? a : b; //Return the greater of the integer and float
     }
 };
 int main() {
-    MaxFinder maxFinder;  // Creating an object of MaxFinder class
-    int int1, int2, int3; // Declare variables to store user input
+    MaxFinder maxFinder;  //Creating an object of MaxFinder class
+    int int1, int2, int3; //Declare variables to store user input
     float float1, float2;
     cout << "Enter two integers: ";
     cin >> int1 >> int2;
